78cf4765dfa437781ec5bb104e175da4a8cda962
inexasli <154554387+inexasli@users.noreply.github.com>
2024-05-08 14:45:51 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.endsWith('/income.html')) {
+    event.respondWith(
+      fetch('https://inexasli.com/income.html')
+    );
+  }
+});
