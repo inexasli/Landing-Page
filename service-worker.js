0a3407b571dbf9d838f1a53848be75026a685668
inexasli <154554387+inexasli@users.noreply.github.com>
2024-05-08 19:13:33 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-/* self.addEventListener('fetch', function(event) {
-  if (event.request.url.endsWith('/income.html')) {
-    event.respondWith(
-      fetch('https://inexasli.com/summary.html')
-    );
-  }
-});*/
