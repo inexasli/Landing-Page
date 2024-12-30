82b58cf79c8235aad87c025a377d7bde42eddb39
adminx123 <154554387+adminx123@users.noreply.github.com>
2024-12-29 17:27:13 -0800

    Update intro.js

--- server/scripts/intro.js
+++ server/scripts/intro.js
@@ -1,4 +1,21 @@
- const romanticincome = document.querySelector('#romanticincome')
+function setCookie(name, value, days) {
+  var expires = "";
+  if (value === undefined || value === null || value === '') {
+    value = '0';
+  }
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+
+  document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/; SameSite=Strict; Secure";
+}
+    
+
+
+
+const romanticincome = document.querySelector('#romanticincome')
 
       romanticincome.addEventListener('change', function() {
         if (romanticincome.checked) {
