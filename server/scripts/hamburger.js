69f78c602d10fdaa18a394a61f277e5327804d0d
adminx123 <154554387+adminx123@users.noreply.github.com>
2024-12-31 19:21:40 -0800

    Update hamburger.js

--- server/scripts/hamburger.js
+++ server/scripts/hamburger.js
@@ -1,1 +1,45 @@
 
+ // Hamburger menu toggle
+      const menuToggle = document.getElementById("menuToggle");
+      const dropdownMenu = document.getElementById("dropdownMenu");
+
+      menuToggle.addEventListener("click", () => {
+        menuToggle.classList.toggle("active");
+        dropdownMenu.style.display =
+          dropdownMenu.style.display === "block" ? "none" : "block";
+      });
+
+      document.addEventListener("click", (event) => {
+        if (
+          !menuToggle.contains(event.target) &&
+          !dropdownMenu.contains(event.target)
+        ) {
+          dropdownMenu.style.display = "none";
+          menuToggle.classList.remove("active");
+        }
+      });
+
+      
+
+      // Function to handle password protection
+      function handlePasswordProtection(url) {
+        const password = prompt("Please enter the password for access:");
+        if (password === "incomeiq") {
+          window.location.href = url;
+        } else {
+          alert("Incorrect password. Access denied.");
+        }
+      }
+
+      // Event listener for the FHE element
+      const FHE = document.getElementById("FHE");
+      FHE.addEventListener("click", function (event) {
+        event.preventDefault();
+        handlePasswordProtection("xxxx.html");
+      });
+
+      const newElement = document.getElementById("ai");
+      newElement.addEventListener("click", function (event) {
+        event.preventDefault();
+        handlePasswordProtection("ai/form.html"); // Replace with the actual URL you want to redirect to
+      });
