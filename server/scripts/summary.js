11526e9908b01476b3eaeacdc3668a6f02c87520
adminx123 <154554387+adminx123@users.noreply.github.com>
2025-01-14 20:03:16 -0800

    Update summary.js

--- server/scripts/summary.js
+++ server/scripts/summary.js
@@ -359,7 +359,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('goalAmount').addEventListener('input', calculateGoal);
 
 function calculateGoal() {
-    const DISPOSABLEINCOME = document.getElementById('DISPOSABLEINCOME').value;
+    const DISPOSABLEINCOME = document.getElementById('DISPOSABLEINCOME');
     // Retrieve the goal amount from the input field
     const goalAmount = document.getElementById('goalAmount').value;
     const parsedGoalAmount = parseFloat(goalAmount);
